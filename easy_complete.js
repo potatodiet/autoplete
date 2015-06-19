f50223da49850ee7400aa2d9c351f6b295bb9d48
Justin Harrison <justin@matthin.com>
2015-06-19 17:04:33 -0400

    Use new class keyword

--- easy_complete.js
+++ easy_complete.js
@@ -1,79 +1,79 @@
 "use strict";
 
-function EasyComplete(input, config) {
-  this.input = input;
-  this.matches = [];
-  this.possibleMatches = [];
+class EasyComplete {
+  constructor(input, config) {
+    this.input = input;
+    this.matches = [];
+    this.possibleMatches = [];
 
-  config = config || {};
-  if (config["list"]) {
-    this.setupList(config["list"]);
-  } else {
-    this.setupList();
-  }
-
-  this.input.addEventListener("input", function inputModified(ev) {
-    this.clearList();
-    
-    if (!ev.srcElement.value) {
-      return;
+    config = config || {};
+    if (config["list"]) {
+      this.setupList(config["list"]);
+    } else {
+      this.setupList();
     }
 
-    for (var i = 0, length = this.possibleMatches.length; i < length; ++i) {
-      var entry = this.possibleMatches[i];
-      if (this.filter(ev.srcElement.value, entry.innerText)) {
-        this.addMatch(entry);
+    this.input.addEventListener("input", function inputModified(ev) {
+      this.clearList();
+      
+      if (!ev.srcElement.value) {
+        return;
       }
-    }
-  }.bind(this));
 
-  this.input.addEventListener("keydown", function keyOnInputPressed(ev) {
-    switch (ev.keyCode) {
-    case 13:
-      this.selectMatch(this.findMatch());
-      break;
-    case 38:
-      this.traverseList(this.TraverseDirection.UP);
-      break;
-    case 40:
-      this.traverseList(this.TraverseDirection.DOWN);
-      break;
-    default:
-      break;
-    }
-  }.bind(this));
-}
+      for (var i = 0, length = this.possibleMatches.length; i < length; ++i) {
+        var entry = this.possibleMatches[i];
+        if (this.filter(ev.srcElement.value, entry.innerText)) {
+          this.addMatch(entry);
+        }
+      }
+    }.bind(this));
 
-EasyComplete.prototype = {
+    this.input.addEventListener("keydown", function keyOnInputPressed(ev) {
+      switch (ev.keyCode) {
+      case 13:
+        this.selectMatch(this.findMatch());
+        break;
+      case 38:
+        this.traverseList(this.TraverseDirection.UP);
+        break;
+      case 40:
+        this.traverseList(this.TraverseDirection.DOWN);
+        break;
+      default:
+        break;
+      }
+    }.bind(this));
+  }
+  
   // Converts a possible match into a real match.
-  addMatch: function(match) {
+  addMatch(match) {
     match.classList.add("visible");
     this.matches.push(match);
-  },
+  }
 
   // Converts a real match back into a possible match.
-  removeMatch: function(match) {
+  removeMatch(match) {
     match.classList.remove("visible");
     var index = this.matches.indexOf(match);
     if (index > -1) {
       this.matches.splice(index, 1);
     }
-  },
+  }
 
-  findMatch: function() {
+  findMatch() {
     for (var i = 0, length = this.matches.length; i < length; ++i) {
       if (this.matches[i].classList.contains("active")) {
         return this.matches[i];
       }
     }
-  },
+  }
 
-  selectMatch: function(match) {
+  selectMatch(match) {
     this.input.value = match.innerText;
     this.clearList();
-  },
+  }
 
-  setupList: function(rawList) {
+  setupList(rawList) {
     var container = document.createElement("span");
     this.input.parentNode.insertBefore(container, this.input);
     container.appendChild(this.input);
@@ -94,16 +94,16 @@ EasyComplete.prototype = {
       this.possibleMatches[i].innerHTML = rawList[i];
       this.list.appendChild(this.possibleMatches[i]);
     }
-  },
+  }
 
-  clearList: function() {
+  clearList() {
     for (var i = this.matches.length - 1; i >= 0; --i) {
       this.removeMatch(this.matches[i]);
     }
     this.matches.length = 0;
-  },
+  }
 
-  traverseList: function(direction) {
+  traverseList(direction) {
     var currentListItem = this.list.getElementsByClassName("active")[0];
 
     switch (direction) {
@@ -126,29 +126,30 @@ EasyComplete.prototype = {
     if (currentListItem) {
       currentListItem.classList.remove("active");
     }
-  },
+  }
 
-  filter: function(input, possibleMatch) {
+  filter(input, possibleMatch) {
     return possibleMatch.search(new RegExp(input, "i")) !== -1;
-  },
+  }
 
-  mergeHash: function(first, second) {
+  mergeHash(first, second) {
     for (var key in second) {
       first[key] = second[key];
     }
-  },
-
-  TraverseDirection: {
-    UP: 0,
-    DOWN: 1
   }
+}
+
+// I'm not sure how to properly define static variables.
+EasyComplete.prototype.TraverseDirection = {
+  UP: 0,
+  DOWN: 1
 }
 
 // For some reason the self-executing function needs to be held in a variable.
 // 'this' doesn't work unless it's held in a variable.
 var easyCompleteInit = (function() {
   // Initialize all standard inputs.
-  var inputs = document.getElementsByTagName("input");
+  let inputs = document.getElementsByTagName("input");
   for (var i = 0, length = inputs.length; i < length; ++i) {
     if (inputs[i].getAttribute("data-list") !== null) {
       new EasyComplete(inputs[i]);
--- example.js
+++ example.js
@@ -1,14 +1,17 @@
-var example = (function() {
-  function CustomEasyComplete(input, config) {
-    EasyComplete.call(this, input, config);
-  }
-  CustomEasyComplete.prototype = Object.create(EasyComplete.prototype);
-  CustomEasyComplete.constructor = CustomEasyComplete;
-  CustomEasyComplete.prototype.filter = function(input, possibleMatch) {
-    return possibleMatch.search(new RegExp(input)) !== -1;
+"use strict";
+
+let example = (function() {
+  class CustomEasyComplete extends EasyComplete {
+    constructor(input, config) {
+      super(input, config);
+    }
+
+    filter(input, possibleMatch) {
+      return possibleMatch.search(new RegExp(input)) !== -1;
+    }
   }
 
-  var input = document.getElementById("example");
+  let input = document.getElementById("example");
   new CustomEasyComplete(input, {
     list: ["Java", "JavaScript", "Ruby"]
   });

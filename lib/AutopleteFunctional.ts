c3fd6f14724219483b3bd21a03e998dd1d9c654f
Justin Harrison <justin@matthin.com>
2016-02-08 00:40:25 -0500

    Support keyboard usage

--- lib/AutopleteFunctional.ts
+++ lib/AutopleteFunctional.ts
@@ -19,14 +19,39 @@ function createAutoplete(input: HTMLInputElement): void {
   });
 
   input.addEventListener('input', ev => addMatches(list, input));
+
+  input.addEventListener('keydown', ev => {
+    switch (ev.keyCode) {
+      case 13:
+        selectMatch(getActiveMatch(list), input);
+        list.classList.remove('visible');
+        break;
+      case 38:
+        ev.preventDefault();
+        traverseList('up', list);
+        break;
+      case 40:
+        ev.preventDefault();
+        traverseList('down', list);
+        break;
+    }
+  });
 }
 
 function addMatches(list: HTMLElement, input: HTMLInputElement) {
+  let anyMatches: boolean = false;
+
   for (let i = 0; i < list.childNodes.length; ++i) {
     const possibleMatch = <HTMLElement>list.childNodes[i];
 
     if (filter(input.value, possibleMatch.textContent)) {
       toggleMatch(possibleMatch, true);
+
+      // Sets the first match as active.
+      if (!anyMatches) {
+        anyMatches = true;
+        setActiveMatch(possibleMatch, list);
+      }
     } else {
       toggleMatch(possibleMatch, false);
     }
@@ -82,9 +107,35 @@ function setActiveMatch(match: HTMLElement, list: HTMLElement): void {
   match.classList.add('active');
 }
 
+function getActiveMatch(list: HTMLElement): HTMLElement {
+  for (let i = 0, length = list.children.length; i < length; ++i) {
+    const match = list.children[i];
+    if (match.classList.contains('active')) {
+      return <HTMLElement>match;
+    }
+  }
+
+  console.debug('No active match exists');
+  return <HTMLElement>document.createElement('null');
+}
+
 function selectMatch(match: HTMLElement, input: HTMLInputElement): void {
-  input.value = match.innerHTML;
+  input.value = match.textContent;
   input.dispatchEvent(new Event('input'));
+}
+
+function traverseList(direction: string, list: HTMLElement): void {
+  let match: HTMLElement = getActiveMatch(list);
+
+  if (direction === 'up') {
+    match = <HTMLElement>match.previousSibling;
+  } else if (direction === 'down') {
+    match = <HTMLElement>match.nextSibling;
+  }
+
+  if (match) {
+    setActiveMatch(match, list);
+  }
 }
 
 let inputs = [].slice.call(
